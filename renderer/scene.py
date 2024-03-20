018566863e2bf318b083ea69e964e7f35cdd49c3
mnbvmnbv2 <ferdinand@ytteborg.com>
2024-03-20 09:56:09 +0100

    add animation for activation in renderer, and files

--- renderer/scene.py
+++ renderer/scene.py
@@ -12,18 +12,30 @@ class Scene:
         self.app = app
         self.objects = []
 
+        self.activations = []
+
         # the activation
-        activation_01_path = Path("renderer/src/sub-001_activation-001.npy")
-        self.objects.append(Chunk(self.app, activation_01_path))
+        activation_path = Path("renderer/src/sub-001/run1")
+        for activation in activation_path.iterdir():
+            self.activations.append(Chunk(self.app, activation))
 
         # the brain model
+        self.objects.append(BrainMesh(self.app))
+        # add one more placeholder for mesh (being popped on first iteration)
         self.objects.append(BrainMesh(self.app))
 
         # the skybox
         self.objects.append(SkyboxMesh(self.app))
 
-    def update(self):
-        pass
+        self.t = 0
+
+    def update(self, time):
+        new_time = round(time)
+        if new_time > self.t:
+            self.t = new_time
+            self.objects.pop(0)
+            activation_idx = self.t % len(self.activations)
+            self.objects.insert(0, self.activations[activation_idx])
 
     def render(self):
         for obj in self.objects:
