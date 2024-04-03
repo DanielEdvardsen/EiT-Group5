ac7a60ed6a257e52e183c3c8401f7f01845941fa
Vegardxoxo <vnhenrik@stud.ntnu.no>
2024-04-03 14:48:26 +0200

    Fixed start  and end times

--- renderer/pyqt_render.py
+++ renderer/pyqt_render.py
@@ -24,8 +24,8 @@ from player import Player
 from scene import Scene
 from shader_program import ShaderProgram
 
-MUSIC_DIR = "genres_original/"
-DATASET_DIR = "dataset/"
+MUSIC_DIR = "../genres_original/"
+DATASET_DIR = "../dataset/"
 
 
 class MusicPlayer(QWidget):
@@ -88,9 +88,9 @@ class MusicPlayer(QWidget):
         main_layout.addWidget(animation_group_box)
 
         # voxel engine
-        voxel_widget = VoxelWidget()
-        voxel_widget.setMinimumSize(700, 700)
-        main_layout.addWidget(voxel_widget)
+        #voxel_widget = VoxelWidget()
+        #voxel_widget.setMinimumSize(700, 700)
+        #main_layout.addWidget(voxel_widget)
 
 
         # Media Player Controls
@@ -101,11 +101,11 @@ class MusicPlayer(QWidget):
         self.next_button = QPushButton("Next")
         self.previous_button = QPushButton("Previous")
 
-        controls_layout.addWidget(self.previous_button)
+        #controls_layout.addWidget(self.previous_button)
         controls_layout.addWidget(self.play_button)
         controls_layout.addWidget(self.pause_button)
         controls_layout.addWidget(self.stop_button)
-        controls_layout.addWidget(self.next_button)
+        #controls_layout.addWidget(self.next_button)
 
         # Volume Slider
         self.volume_slider = QSlider(Qt.Horizontal)
@@ -147,8 +147,8 @@ class MusicPlayer(QWidget):
         """
         Update the genre based on the selected radio button.
         """
-        self.genre = self.genre_combo_box.currentText().lower()  # CHANGE: Get the current text from the QComboBox
-        self.load_song_list()  # CHANGE: Load songs for the selected genre
+        self.genre = self.genre_combo_box.currentText().lower()
+        self.load_song_list()
         self.play_song()
 
     def set_volume(self, value):
@@ -184,10 +184,10 @@ class MusicPlayer(QWidget):
             start = row['start']
             end = row['end']
 
-            if genre not in self.song_files:
-                self.song_files[genre] = {}
+            if genre in self.song_files:
+                continue
 
-            self.song_files[genre][index] = {'track': track, 'start': start, 'end': end}
+            self.song_files[genre] = {'track': track, 'start': start, 'end': end}
 
     def play_song(self):
         """
@@ -208,13 +208,25 @@ class MusicPlayer(QWidget):
             return
 
         # Get the first song and pad the string with zeros such that it adheres to the naming convention
-        cur_song = list(self.song_files[self.genre].keys())[0]
-        cur_song = str(cur_song).zfill(5)
+        cur_track = self.song_files[self.genre]['track']
+        cur_track = str(cur_track).zfill(5)
+        start_time = int(self.song_files[self.genre]['start'] * 1000)
 
         # Play the song
-        song_url = QUrl.fromLocalFile(os.path.join(MUSIC_DIR, self.genre, f"{self.genre}.{cur_song}.wav"))
+        song_url = QUrl.fromLocalFile(os.path.join(MUSIC_DIR, self.genre, f"{self.genre}.{cur_track}.wav"))
         self.media_player.setMedia(QMediaContent(song_url))
+        self.media_player.setPosition(start_time)
         self.media_player.play()
+        self.media_player.positionChanged.connect(self.check_song_end)
+
+    def check_song_end(self, position):
+        """
+        Check if the current playback position has reached the end time of the current song and stop playback if so.
+        """
+        end_time_ms = int(self.song_files[self.genre]['end'] * 1000)
+        if position >= end_time_ms:
+            self.media_player.stop()
+            self.media_player.positionChanged.disconnect(self.check_song_end)
 
     def play_next_song(self, forward=True):
         # Check if there are any songs in the list
